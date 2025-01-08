3b05afca6d26ef943988df49d5de7d73a7a9bcf2
Anup Shakya <anupshk39@gmail.com>
2025-01-08 23:40:42 +0545

    Portfolio Header and Hero Section Completed

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="min-h-screen">
+    <!-- Wrap components in Suspense to handle loading -->
+     <Suspense>
+      <template #default>
+        <div class="bg-primary2">
+          <HeroSection/>
+        </div>
+      </template>
+      <template #fallback>
+        <div class="flex justify-center items-center min-h-screen">
+          <LoadingSpinner/>
+        </div>
+      </template>
+     </Suspense>
+  </div>
+  
+</template>
+<script setup>
+import {defineAsyncComponent} from 'vue';
+import LoadingSpinner from './components/LoadingSpinner.vue'
+const HeroSection = defineAsyncComponent(()=>import('./components/HeroSection.vue'));
+</script>
+<style lang="">
+
+</style>
